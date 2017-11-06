89090ba789b9215f784b836a0cf38c317c9f27cb
rafaelsiq <rafaelsqf2@gmail.com>
2017-11-06 15:38:43 -0200

    alteracao rafael

--- Jogo de luta/Jogo de luta/EscolhaPersonagem.Designer.cs
+++ Jogo de luta/Jogo de luta/EscolhaPersonagem.Designer.cs
@@ -115,7 +115,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(574, 66);
+            this.label4.Location = new System.Drawing.Point(574, 63);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(66, 13);
             this.label4.TabIndex = 8;
