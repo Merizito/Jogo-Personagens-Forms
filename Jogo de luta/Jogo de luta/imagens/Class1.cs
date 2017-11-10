168dae9efe4e06649d4f61cc7e0171400ffa2aec
Rafael <rafaelsqf2@gmail.com>
2017-11-09 22:16:33 -0200

    rafael siqueira

---
+++ Jogo de luta/Jogo de luta/imagens/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jogo_de_luta.imagens
+{
+    class Class1
+    {
+    }
+}
