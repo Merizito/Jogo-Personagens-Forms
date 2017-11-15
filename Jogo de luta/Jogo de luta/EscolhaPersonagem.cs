d189087ed0774ac4cf2107f8651285c7e19bcf66
Merizito <juninho.meritus@hotmail.com>
2017-11-15 18:13:33 -0200

    Marco Aurélio - 15/11/2017 (Imagens do escolha alterando em tempo de exec.)

--- Jogo de luta/Jogo de luta/EscolhaPersonagem.cs
+++ Jogo de luta/Jogo de luta/EscolhaPersonagem.cs
@@ -30,8 +30,8 @@ namespace Jogo_de_luta
             AddPersonagem("armas");
             Player1.Text = p1;
             label2.Text = p2;
-          //  ImagemPlayer1.Image = Properties.Resources.soldado;
-            ImagemPlayer2.ImageLocation = "soldado.png";
+            ImagemPlayer1.Image = Properties.Resources.dragao;
+            //ImagemPlayer2.ImageLocation = "soldado.png";
 
         }
 
@@ -99,12 +99,50 @@ namespace Jogo_de_luta
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            
+            string getNome;
+
+            getNome = Convert.ToString(comboBox1.SelectedIndex);    //Dragao = 0 / Soldado =1 / Lutador de sumo = 2 / General = 3.
+            //MessageBox.Show(getNome);
+            if (getNome == "0")                                         //Dragao
+            {
+                ImagemPlayer1.Image = Properties.Resources.dragao;
+            }
+            else if (getNome == "1")                                    //Soldado
+            {
+                ImagemPlayer1.Image = Properties.Resources.soldado;
+            }
+            else if (getNome == "2")                                    //Lutador de Sumo
+            {
+                ImagemPlayer1.Image = Properties.Resources.lutadorsumo;
+            }
+            else if (getNome == "3")                                    //General
+            {
+                ImagemPlayer1.Image = Properties.Resources.general;
+            }
         }
 
         private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
         {
+            string getNome2;
 
+            getNome2 = Convert.ToString(comboBox3.SelectedIndex);    //Dragao = 0 / Soldado =1 / Lutador de sumo = 2 / General = 3.
+            //MessageBox.Show(getNome);
+            if (getNome2 == "0")                                         //Dragao
+            {
+                ImagemPlayer2.Image = Properties.Resources.dragao;
+            }
+            else if (getNome2 == "1")                                    //Soldado
+            {
+                ImagemPlayer2.Image = Properties.Resources.soldado;
+            }
+            else if (getNome2 == "2")                                    //Lutador de Sumo
+            {
+                ImagemPlayer2.Image = Properties.Resources.lutadorsumo;
+            }
+            else if (getNome2 == "3")                                    //General
+            {
+                ImagemPlayer2.Image = Properties.Resources.general;
+            }
         }
  
         private void button1_Click(object sender, EventArgs e)
